a77271207ebaac7e0d8b3068d588ceb57645b057
Rezus <rere.ddd1112@yandex.ru>
2019-09-27 22:52:00 +0300

    LS

---
+++ src/com/company/LS.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LS {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x=in.nextInt();
+        int a=(x%10);
+        System.out.println(a);
+    }
+}
